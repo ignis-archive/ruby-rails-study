79078964e2d4784bfd849fca41d48a37f861ea8e
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-06-05 16:07:40 +0900

    6.2 Adding a Link まで終了

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '873bbee53c43c02d150cf2415bb3ab9e856f0756287cf3daee3da8b0191e5d2e876ee9b1bc86e47360f87c405a6b8ac26c045abfc28f6459e6e651625ad56b42'
